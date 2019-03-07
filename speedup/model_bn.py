029ccd74991cb20fba455930298030d9f17173f3
Mohammed Henni <em_henni@esi.dz>
2019-03-07 17:47:49 +0100

    Added dropout to the model

--- speedup/model_bn.py
+++ speedup/model_bn.py
@@ -11,17 +11,19 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 e = 0.05
 
 class Model_BN(nn.Module):
-    def __init__(self, input_size, output_size, hidden_sizes=[10, 10, 10]):
+    def __init__(self, input_size, output_size, hidden_sizes=[10, 10, 10], drops=[0.4, 0.4, 0.4]):
         super().__init__()
         hidden_sizes = [input_size] + hidden_sizes
 
         self.hidden_layers = nn.ModuleList()
         self.batch_norm_layers = nn.ModuleList()
+        self.dropouts= nn.ModuleList()
 
         for i in range(len(hidden_sizes)-1):
             self.hidden_layers.append(nn.Linear(hidden_sizes[i], hidden_sizes[i+1], bias=False))
             self.batch_norm_layers.append(nn.BatchNorm1d(hidden_sizes[i+1]))
             nn.init.xavier_uniform_(self.hidden_layers[i].weight)
+            self.dropouts = nn.Dropout(drops[i])
 
 
         self.predict = nn.Linear(hidden_sizes[-1], output_size)
@@ -32,7 +34,7 @@ class Model_BN(nn.Module):
     def forward(self, x):
 
         for i in range(len(self.hidden_layers)):
-            x = F.relu(self.batch_norm_layers[i](self.hidden_layers[i](x)))
+            x = self.dropouts[i](F.relu(self.batch_norm_layers[i](self.hidden_layers[i](x))))
 
         x = self.predict(x)
         return x
